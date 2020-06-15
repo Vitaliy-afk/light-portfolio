7e9937de9c8d4769f926f470af5512c26ef9d7b8
Vitaliy <53347040+Vitaliy-afk@users.noreply.github.com>
2020-06-15 13:43:14 +0300

    upd1

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+jQuery( document ).ready(function() {
+    initBurgerMenu();
+  });
+
+
+function initBurgerMenu() {
+    jQuery('.burger').on('click', function(e){
+        e.preventDefault()
+        jQuery(this).toggleClass('open');
+        jQuery('body').toggleClass('burger-active');
+        
+    });
+  }
+
+console.log(2232);
